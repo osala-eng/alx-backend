bf1cb424b06da198464db001aa1a24f1c56a3f15
Jashon Osala <jashonosala@gmail.com>
2023-07-23 15:52:51 +0300

    Format pep

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """Simple helper function."""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return tuple of size two containing a start index and an end index."""
     return ((page - 1) * page_size, page * page_size)
