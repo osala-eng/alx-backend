7c29218c2b5ccdd44dba56cd3fb93bd64b7b64dc
Jashon Osala <jashonosala@gmail.com>
2023-07-23 15:44:26 +0300

    Added simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Simple helper function."""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """Return tuple of size two containing a start index and an end index."""
+    return ((page - 1) * page_size, page * page_size)
+
+
+if __name__ == "__main__":
+    res = index_range(1, 7)
+    print(type(res))
+    print(res)
+
+    res = index_range(page=3, page_size=15)
+    print(type(res))
+    print(res)
